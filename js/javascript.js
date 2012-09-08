58743ef45b110b9911926da726995216e28aa7bf
Matthew Powers <matthewpowers@Matthew-Powerss-iMac.local>
2012-09-08 18:52:58 -0400

    Added navigation bar

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-	$('h1').hide().fadeIn(3000);
+	$('h1').hide().fadeIn(1000);
+	
+	$("#navigation").navPlugin({
+	   'itemWidth': 150,
+	   'itemHeight': 30,
+	   'navEffect': "slide",
+	   'speed': 250
+	}); //end navPlugin
 }); //end ready
